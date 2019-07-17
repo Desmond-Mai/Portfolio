4d3b5e4c68f984a44bf0f654834fc9986c8abdca
Desmond Mai <dmai643@gmail.com>
2019-07-17 11:32:51 -0700

    completed tictactoe

--- tic tac toe/tic tac toe.js
+++ tic tac toe/tic tac toe.js
@@ -2,13 +2,15 @@
 var whoseTurnItIs = 1;
 
 var board = ["", "", "", "", "", "", "", "", ""]
+var wins = false 
+
 //"" is blank
 //blue "blue"
 //red: "red"
 
 function changeColor(block, num) {
     var color = document.querySelector(block).style.backgroundColor;
-    if (color != "green" && color != "blue") {
+    if (color != "green" && color != "blue" && wins == false) {
         if (whoseTurnItIs == 1) {
             board[num] = "blue";
             document.querySelector(block).style.backgroundColor = "blue";
@@ -24,46 +26,63 @@ function changeColor(block, num) {
         console.log(board)
         checkwin("blue");
         checkwin("green");
-        
+
     }
+    
 }
 function checkwin(player) {
     // check if top row wins
     if (board[0] == board[1] && board[2] == board[1] && board[0] == player) {
-        console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     // checks if left vertical row wins
     else if (board[0] == board[3] && board[6] == board[3] && board[0] == player)  {
-        console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     // checks if middle row wins
     else if (board[3] == board[4] && board[5] == board[4] && board[3] == player) {
-        console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     // check if middle vertical row wins
     else if (board[1] == board[4] && board[7] == board[4] && board[1] == player) {
-        console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     // check if right vertical row wins
     else if (board[2] == board[5] && board [8] == board[5] && board[2] == player) {
-         console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     // bottom row wins
     else if (board[6] == board[7] && board[8] == board[7] && board[6] == player){
-        console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     //right diaganol wins
     else if (board[0] == board[4] && board[8] == board[4] && board[0] == player){
-        console.log(player + " wins")
+        wins = true
+        displayMessage(player);
     }
     //left diaganol wins
     else if (board[2] == board[4] && board[6] == board[4] && board[2] == player) {
-        console.log(player + "wins")
+        wins = true
+        displayMessage(player);
     }
     
-
-
 }
+
+function displayMessage(player) {
+    var message = document.createElement("H2");
+    message.setAttribute("id","message");
+    message.innerHTML = player + " wins!";
+    var body = document.querySelector("body");
+    body.appendChild(message);
+    }
+
+
 
 document.querySelector("#top-left").onclick = function () {
     changeColor("#top-left", 0);
