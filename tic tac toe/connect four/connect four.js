5af6b29489a0754a029c2870fbbd694404dc5efe
Desmond Mai <dmai643@gmail.com>
2019-07-22 09:12:52 -0700

    demo games

--- tic tac toe/connect four/connect four.js
+++ tic tac toe/connect four/connect four.js
@@ -1,7 +1,7 @@
 // var user =  
 var whoseTurnItIs = 1;
 
-var board = ["", "", "", "", "", "", "", "", ""]
+var board = ["", "", "", "", "", "", "", "", "","","","","","","","","","","",""]
 var wins = false
 
 //"" is blank
@@ -32,7 +32,7 @@ function changeColor(block, num) {
 }
 function checkwin(player) {
     // check if top row wins
-    if (board[0] == board[1] && board[2] == board[1] && board[0] == player) {
+    if (board[0] == board[1] && board[7] && board[2] == board[1] && board[0] == player) {
         wins = true
         displayMessage(player);
     }
@@ -81,46 +81,4 @@ function displayMessage(player) {
     var body = document.querySelector("body");
     body.appendChild(message);
 }
-
-
-/*
-document.querySelector("#top-left").onclick = function () {
-    changeColor("#top-left", 0);
-    //top-left is
-}
-
-
-document.querySelector("#top-middle").onclick = function () {
-    changeColor("#top-middle", 1);
-}
-
-document.querySelector("#top-right").onclick = function () {
-    changeColor("#top-right", 2);
-}
-document.querySelector("#middle-left").onclick = function () {
-    changeColor("#middle-left", 3);
-}
-
-document.querySelector("#middle-middle").onclick = function () {
-    changeColor("#middle-middle", 4);
-}
-document.querySelector("#middle-right").onclick = function () {
-    changeColor("#middle-right", 5);
-
-}
-document.querySelector("#bottom-left").onclick = function () {
-    changeColor("#bottom-left", 6);
-}
-
-document.querySelector("#bottom-middle").onclick = function () {
-    changeColor("#bottom-middle", 7);
-}
-
-document.querySelector("#bottom-right").onclick = function () {
-    changeColor("#bottom-right", 8);
-}
-
-*/
-
-var turns = 9;
-
+var turns = 13;
