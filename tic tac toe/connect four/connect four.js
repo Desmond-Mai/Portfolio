c2672ec115a057157414c1313d4b78a6d0d4be98
Desmond Mai <dmai643@gmail.com>
2019-07-23 10:59:26 -0700

    allthe rows done

--- tic tac toe/connect four/connect four.js
+++ tic tac toe/connect four/connect four.js
@@ -1,7 +1,7 @@
 // var user =  
 var whoseTurnItIs = 1;
 
-var board = ["", "", "", "", "", "", "", "", "","","","","","","","","","","",""]
+var board = ["", "", "", "", "", "", "", "", "","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""]
 var wins = false
 
 //"" is blank
@@ -32,42 +32,105 @@ function changeColor(block, num) {
 }
 function checkwin(player) {
     // check if top row wins
-    if (board[0] == board[1] && board[7] && board[2] == board[1] && board[0] == player) {
+    if (board[0] == player && board[1] == player && board[2] == player && board[3] == player) {
         wins = true
         displayMessage(player);
     }
     // checks if left vertical row wins
-    else if (board[0] == board[3] && board[6] == board[3] && board[0] == player) {
+    else if (board[1] == player && board[2] == player && board[3] == player && board[4] == player) {
         wins = true
         displayMessage(player);
     }
     // checks if middle row wins
-    else if (board[3] == board[4] && board[5] == board[4] && board[3] == player) {
+    else if (board[2] == player && board[3] == player && board[4] == player && board[5] == player) {
         wins = true
         displayMessage(player);
     }
     // check if middle vertical row wins
-    else if (board[1] == board[4] && board[7] == board[4] && board[1] == player) {
-        wins = true
+    else if (board[3] == player && board[4] == player && board[5] == player && board[6] == player) {
         displayMessage(player);
     }
     // check if right vertical row wins
-    else if (board[2] == board[5] && board[8] == board[5] && board[2] == player) {
+    else if (board[7] == player && board[8] == player && board[9] == player && board[10] == player) {
         wins = true
         displayMessage(player);
     }
     // bottom row wins
-    else if (board[6] == board[7] && board[8] == board[7] && board[6] == player) {
+    else if (board[8] == player && board[9] == player && board[10] == player && board[11] == player) {
         wins = true
         displayMessage(player);
     }
-    //right diaganol wins
-    else if (board[0] == board[4] && board[8] == board[4] && board[0] == player) {
-        wins = true
-        displayMessage(player);
-    }
+
     //left diaganol wins
-    else if (board[2] == board[4] && board[6] == board[4] && board[2] == player) {
+    else if (board[9] == player && board[10] == player && board[11] == player && board[12] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[10] == player && board[11] == player && board[12] == player && board[13] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[14] == player && board[15] == player && board[16] == player && board[17] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[15] == player && board[16] == player && board[17] == player && board[18] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[16] == player && board[17] == player && board[18] == player && board[19] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[17] == player && board[18] == player && board[19] == player && board[20] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[21] == player && board[22] == player && board[23] == player && board[24] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[22] == player && board[23] == player && board[24] == player && board[25] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[23] == player && board[24] == player && board[25] == player && board[26] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[24] == player && board[25] == player && board[26] == player && board[27] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[28] == player && board[29] == player && board[30] == player && board[31] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[29] == player && board[30] == player && board[31] == player && board[32] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[30] == player && board[31] == player && board[32] == player && board[33] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[31] == player && board[32] == player && board[33] == player && board[34] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[35] == player && board[36] == player && board[37] == player && board[38] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[36] == player && board[37] == player && board[38] == player && board[39] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[37] == player && board[38] == player && board[39] == player && board[40] == player) {
+        wins = true
+        displayMessage(player);
+    }
+    else if (board[38] == player && board[39] == player && board[40] == player && board[41] == player) {
         wins = true
         displayMessage(player);
     }
